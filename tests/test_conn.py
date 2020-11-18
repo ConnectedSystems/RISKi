36925b3f3b48d7a143677d619232233be7b3e88f
ConnectedSystems <takuyai@gmail.com>
2020-11-19 02:15:08 +1100

    Use newly created dev server

--- tests/test_conn.py
+++ tests/test_conn.py
@@ -5,9 +5,10 @@ import riski as ri
 
 def test_insertion():
     csv_fn = "data/CSVs/SWIO_COM_EQ_Shake_RP.csv"
-    r_conn = ri.RDLConnection(".settings.yaml", dev=False)
+    r_conn = ri.RDLConnection(".settings.yaml", dev=True)
 
     r_conn.insert_csv_data(csv_fn)
+    # r_conn.run_query("SELECT * FROM tablename")
 
 
 if __name__ == '__main__':
