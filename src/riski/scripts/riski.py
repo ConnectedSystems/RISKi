1d08ab41e57a5714e85b0b7e65c61d0bcf717fee
ConnectedSystems <takuyai@gmail.com>
2020-11-21 16:49:31 +1100

    DB interaction commands

--- src/riski/scripts/riski.py
+++ src/riski/scripts/riski.py
@@ -6,7 +6,8 @@ import os
 
 import typer
 
-from riski._utils import load_settings
+import riski as ri
+from riski._utils import load_settings, generate_config
 
 
 app = typer.Typer()
@@ -18,11 +19,12 @@ def setup_dev_db(settings_file: str):
     data_settings = settings['rdl-data']
 
     sql_dir = data_settings['sql']
-    sql_files = glob.glob(f"{sql_dir}/*.sql")
+    sql_files = glob.glob(f"{sql_dir}*.sql")
     sql_files = [glob.escape(s) for s in sql_files]
 
-    psql = settings['database']['psql']
+    
     dev_settings = settings['database']['dev']
+    psql = dev_settings['psql']
 
     # Find user creation script
     user_script = "create-users.sql"
@@ -50,6 +52,17 @@ def setup_dev_db(settings_file: str):
     for script in sql_files:
         cmd = base_cmd + [f"-f{script}"]
         subprocess.run(cmd)
+
+
+@app.command()
+def create_rdl_data_config(settings_file: str, dev: bool = False):
+    generate_config(settings_file, dev=dev)
+
+
+@app.command()
+def import_hazard(settings_file: str, csv_fn, json_fn):
+    r_conn = ri.RDLConnection(".settings.yaml", dev=False)
+    r_conn.import_hazard_event(csv_fn, json_fn)
 
 
 @app.command()
